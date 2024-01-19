b7c16fdb977fa765448af655f37d1542e91cca61
Feat <rksk8888@naver.com>
2024-01-19 12:00:56 +0900

    start

---
+++ src/main/java/com/kimoi/nomore/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.kimoi.nomore.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class MainController {
+
+
+    @GetMapping("/api/test")
+    public Map<String, String> testMethod(@RequestParam Map<String, String> response) {
+        response.put("data", "springboot to react");
+        return response;
+    }
+    
+    
+}
---
+++ src/test/java/com/kimoi/nomore/NomoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimoi.nomore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
