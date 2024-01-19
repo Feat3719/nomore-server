633380e7173c08172dfadea2cea2e214979abf8e
Feat <rksk8888@naver.com>
2024-01-19 16:05:02 +0900

    domain-update

--- src/main/java/com/kimoi/nomore/domain/Order.java
+++ src/main/java/com/kimoi/nomore/domain/Order.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(name = "ORDER")
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
