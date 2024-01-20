1bac7d768812a648d7c75fd40ef97408864684b0
Feat <rksk8888@naver.com>
2024-01-20 21:12:51 +0900

    maincommit

--- src/main/java/com/kimoi/nomore/service/EmailService.java
+++ src/main/java/com/kimoi/nomore/service/EmailService.java
@@ -1,74 +1,75 @@
-// package com.kimoi.nomore.service;
+package com.kimoi.nomore.service;
 
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.mail.javamail.JavaMailSender;
-// import org.springframework.mail.javamail.MimeMessageHelper;
-// import org.springframework.stereotype.Service;
-// import org.thymeleaf.context.Context;
-// import org.thymeleaf.TemplateEngine;
-// import server.email.entity.EmailMessage;
-// import com.kimoi.nomore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.TemplateEngine;
+import com.kimoi.nomore.dto.email.Email;
+import com.kimoi.nomore.service.UserService;
 
-// import javax.mail.MessagingException;
-// import javax.mail.internet.MimeMessage;
-// import java.util.Random;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
-// @Slf4j
-// @Service
-// @RequiredArgsConstructor
-// public class EmailService {
+import java.util.Random;
 
-//     private final JavaMailSender javaMailSender;
-//     private final TemplateEngine templateEngine;
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
 
-//     private final UserService userService;
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
 
-//     public String sendMail(EmailMessage emailMessage, String type) {
-//         String authNum = createCode();
+    private final UserService userService;
 
-//         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    public String sendMail(Email email, String type) {
+        String authNum = createCode();
 
-//         if (type.equals("password")) userService.SetTempPassword(emailMessage.getTo(), authNum);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
-//         try {
-//             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-//             mimeMessageHelper.setTo(emailMessage.getTo()); // 메일 수신자
-//             mimeMessageHelper.setSubject(emailMessage.getSubject()); // 메일 제목
-//             mimeMessageHelper.setText(setContext(authNum, type), true); // 메일 본문 내용, HTML 여부
-//             javaMailSender.send(mimeMessage);
+        if (type.equals("password")) userService.SetTempPassword(email.getTo(), authNum);
 
-//             log.info("Success");
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(email.getTo()); // 메일 수신자
+            mimeMessageHelper.setSubject(email.getSubject()); // 메일 제목
+            mimeMessageHelper.setText(setContext(authNum, type), true); // 메일 본문 내용, HTML 여부
+            javaMailSender.send(mimeMessage);
 
-//             return authNum;
+            log.info("Success");
 
-//         } catch (MessagingException e) {
-//             log.info("fail");
-//             throw new RuntimeException(e);
-//         }
-//     }
+            return authNum;
 
-//     // 인증번호 및 임시 비밀번호 생성 메서드
-//     public String createCode() {
-//         Random random = new Random();
-//         StringBuffer key = new StringBuffer();
+        } catch (MessagingException e) {
+            log.info("fail");
+            throw new RuntimeException(e);
+        }
+    }
 
-//         for (int i = 0; i < 8; i++) {
-//             int index = random.nextInt(4);
+    // 인증번호 및 임시 비밀번호 생성 메서드
+    public String createCode() {
+        Random random = new Random();
+        StringBuffer key = new StringBuffer();
 
-//             switch (index) {
-//                 case 0: key.append((char) ((int) random.nextInt(26) + 97)); break;
-//                 case 1: key.append((char) ((int) random.nextInt(26) + 65)); break;
-//                 default: key.append(random.nextInt(9));
-//             }
-//         }
-//         return key.toString();
-//     }
+        for (int i = 0; i < 8; i++) {
+            int index = random.nextInt(4);
 
-//     // thymeleaf를 통한 html 적용
-//     public String setContext(String code, String type) {
-//         Context context = new Context();
-//         context.setVariable("code", code);
-//         return templateEngine.process(type, context);
-//     }
-// }
+            switch (index) {
+                case 0: key.append((char) ((int) random.nextInt(26) + 97)); break;
+                case 1: key.append((char) ((int) random.nextInt(26) + 65)); break;
+                default: key.append(random.nextInt(9));
+            }
+        }
+        return key.toString();
+    }
+
+    // thymeleaf를 통한 html 적용
+    public String setContext(String code, String type) {
+        Context context = new Context();
+        context.setVariable("code", code);
+        return templateEngine.process(type, context);
+    }
+}
